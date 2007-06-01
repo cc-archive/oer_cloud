c23981f9858e219cb74c28763950adc87109e75d
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-06-01 12:59:06 +0000

    Committing OAI import script.
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/ccoer/trunk@5909 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ scripts/cc_oer/import_scripts.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import md5
+import datetime
+
+import lxml.etree as et
+
+def nsta():
+    """Generate output conforming to the del.icio.us export schema suitable
+    for import into the CC oercloud system."""
+
+    NSMAP = dict(oai = 'http://www.openarchives.org/OAI/2.0/',
+                 oai_dc = 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+                 dc = 'http://purl.org/dc/elements/1.1/')
+
+    in_file = et.parse(sys.argv[-1])
+    result = et.Element("posts", {'user':'nsta'})
+
+    for record in in_file.xpath('//oai:record', NSMAP):
+
+        title = record.xpath('./oai:metadata/oai_dc:dc/dc:title', NSMAP)[0].text
+        desc = record.xpath('./oai:metadata/oai_dc:dc/dc:description', NSMAP)[0].text
+        url = record.xpath('./oai:metadata/oai_dc:dc/dc:identifier', NSMAP)[0].text
+        tags = [n.text for n in record.xpath('./oai:metadata/oai_dc:dc/dc:subject', NSMAP)]
+
+        et.SubElement(result, 'post', dict(
+                href = url,
+                description = title,
+                extended = desc,
+                tag = " ".join([n.replace(' ','_') for n in tags]),
+                time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                hash = md5.new(url).hexdigest()
+                )
+                      )
+
+    print et.tostring(result)
+
+
