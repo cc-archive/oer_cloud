e1bf41cdc44dcdc2b54d604c02943d75729d788c
nkinkade <nkinkade@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-07-25 22:27:54 +0000

    Fixed a small problem in activate.php.
    git-svn-id: https://cctools.svn.sourceforge.net/svnroot/cctools/ccoer/trunk@6361 aec4f576-4910-0410-beeb-ecb0427469e7

--- www/activate.php
+++ www/activate.php
@@ -23,12 +23,12 @@ if ( isset($_GET['key']) && ! empty($_GET['key']) ) {
 				$user['uId']
 			);
 			if ( $qid = $userservice->db->sql_query($sql) ) {
-				$tplVars['msg'] = "Thank you.  Your account has been activated.  You may now <a href='$root/login/'>login</a>.";
+				$tplVars['msg'] = "Thank you.  Your account has been activated.  You may now <a href='{$GLOBALS['root'}login/'>login</a>.";
 			} else {
 				$tplVars['error'] = "There was an error activating your account.  Please contact the site administrator.";
 			}
 		} else {
-			$tplVars['error'] = "This account was already activated.  You may <a href='$root/login/'>login</a>.";
+			$tplVars['error'] = "This account was already activated.  You may <a href='{$GLOBALS['root']}login/'>login</a>.";
 		}
 	} else {
 		$tplVars['error'] = "The activation key specified was not found in the database.";
